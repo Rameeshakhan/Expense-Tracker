9871709a1ebbafc9739f48934be093617307dfb9
Rameesha Khan <rameeshakhan75@gmail.com>
2023-06-02 22:23:51 +0500

    Firebase Configuration Added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,58 @@
- // JavaScript code to create the typing effect
- const heading = document.getElementById('typing-heading');
- const text = "Manage Your Finance";
- let index = 0;
+// //firebase authentication
+// // const app = firebase.initializeApp(firebaseConfig);
+// document.addEventListener("DOMContentLoaded", function() {
+// const username = document.getElementById("signup-username")
+// const email = document.getElementById("signup-email")
+// const password = document.getElementById("signup-password")
+// const signupForm = document.getElementById("signup-form")
 
- function typeEffect() {
-   if (index < text.length) {
-     heading.innerHTML += text.charAt(index);
-     index++;
-     setTimeout(typeEffect, 200); // Adjust the typing speed by changing the timeout value (in milliseconds)
-   }
- }
- typeEffect();
+// signupForm.addEventListener("submit" , signUp)
+
+// function signUp(e) {
+//   e.preventDefault();
+//   const usernameValue = username.value;
+//   const emailValue = email.value;
+//   const passwordValue = password.value;
+
+//   // const app = initializeApp(firebaseConfig);
+//   firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+//     .then((userCredential) => {
+//       // Sign-up successful
+//       const user = userCredential.user;
+//       console.log("Sign-up successful!", user);
+//       // Additional logic after successful sign-up
+//     })
+//     .catch((error) => {
+//       // Sign-up failed
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.error("Sign-up failed", errorCode, errorMessage);
+//       // Handle sign-up error, display error message, etc.
+//     });
+// }
+
+// });
+
+// Get the sign-up form element
+const signupForm = document.getElementById("signup-form");
+
+// Add an event listener to the form
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get user input values
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
+
+  // Create a new user with email and password
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Sign-up successful, you can redirect the user or perform other actions here
+      console.log("Sign up successful!");
+    })
+    .catch((error) => {
+      // Handle sign-up errors
+      console.log(error.message);
+    });
+});
+
