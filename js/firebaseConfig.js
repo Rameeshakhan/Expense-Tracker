00ba8531412619ba805916c381640471e5f57b73
Rameesha Khan <rameeshakhan75@gmail.com>
2023-06-02 22:23:19 +0500

    firebase configuration added

---
+++ js/firebaseConfig.js
@@ -0,0 +1,11 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyBaPrwDh0RR48u7Xj7A6VrOjjJm7oZ_ajc",
+    authDomain: "expense-tracker-e3a72.firebaseapp.com",
+    projectId: "expense-tracker-e3a72",
+    storageBucket: "expense-tracker-e3a72.appspot.com",
+    messagingSenderId: "340499628027",
+    appId: "1:340499628027:web:3001a0296ea99bc788155e",
+    measurementId: "G-CP1H6TG2WM"
+  };
+
+  // firebase.initializeApp(firebaseConfig);
