96d27f7e948554dc18b5635ee5069c94e5e5fba2
Rameesha Khan <rameeshakhan75@gmail.com>
2023-06-04 19:05:21 +0500

    Logout Functionality Added

--- js/logout.js
+++ js/logout.js
@@ -1,34 +1,17 @@
-import app from "../js/firebaseConfig.js"    
-import {getAuth ,  signOut} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js"
+import app from "../js/firebaseConfig.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-const logoutButton = document.querySelector("#logout")
-logoutButton.addEventListener("click", logoutUser)
-
-let userIsLoggedOut = false
-
-// Logout function
-function logoutUser(userIsLoggedOut) {
-    signOut(auth)
-      .then(() => {
-        console.log("User logged out successfully");
-        window.location.href = "../pages/login.html";
-        userIsLoggedOut = true
-      })
-      .catch((error) => {
-        console.log("Logout error:", error.message);
-      });
-    }
-    
-
-if(userIsLoggedOut) {
-  sessionStorage.clear();
-  history.replaceState(null, null, location.href);
-  window.onpopstate = function () {
-    if (sessionStorage.getItem('isLoggedIn')) {
-      history.go(1);
-    }
-  };
-  }
-  
+const logoutButton = document.getElementById("logoutButton");
+logoutButton.addEventListener("click", logoutUser);
+function logoutUser() {
+  signOut(auth)
+    .then(() => {
+      console.log("User logged out successfully");
+      window.location.href = "../pages/login.html";
+    })
+    .catch((error) => {
+      console.log("Logout error:", error.message);
+    });
+}
